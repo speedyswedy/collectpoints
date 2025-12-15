4d54e8f0f48c564c370518131e2913cb262208f3
speedyswedy <speedyswedy@gmail.com>
2025-12-15 22:37:17 +0000

    sdfsfd

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
+  console.log("Hello world function triggered!");
   response.send("Hello from Firebase!");
 });
